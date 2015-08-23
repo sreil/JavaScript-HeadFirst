b57dcbdcd5b60cd8fd83914b765916f0dd659ff1
sreil <seanpreilly@gmail.com>
2015-08-22 21:42:36 -0600

    add be the browser

---
+++ Chapter 1/bethebrowser1.js
@@ -0,0 +1,13 @@
+//Test for jokes
+var joke = "Javascript walked into a bar...";
+var toldJoke = false;
+var punchline = "Better watch out for those semi-colons.";
+var entage = 20;
+var result;
+
+if (toldJoke == true) {
+	alert(punchline);
+}
+else {
+	alert(joke);
+}
