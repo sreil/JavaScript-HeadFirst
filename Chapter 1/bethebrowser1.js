ed9fab0c9ba612778133892f8fcd89c75a28d307
sreil <seanpreilly@gmail.com>
2015-08-22 21:54:46 -0600

    update btb1

--- Chapter 1/bethebrowser1.js
+++ Chapter 1/bethebrowser1.js
@@ -11,3 +11,14 @@ if (toldJoke == true) {
 else {
 	alert(joke);
 }
+
+//Movie Night
+var zipCode = 98104;
+var joesFavoriteMovie = "Forbidden Planet";
+var movieTickets = 9;
+
+if (movieTickets >= 9) {
+	alert("Too much!");
+} else {
+	alert("We're going to see " + joesFavoriteMovie);
+}
