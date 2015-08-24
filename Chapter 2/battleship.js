663c812aeff50142f9f4ac606c76c56d629cbf6e
sreil <seanpreilly@gmail.com>
2015-08-23 22:06:31 -0600

    check if ship is sunk

--- Chapter 2/battleship.js
+++ Chapter 2/battleship.js
@@ -10,10 +10,24 @@ var isSunk = false;
 
 while(isSunk == false) {
 	guess = prompt("Ready, aim, fire! (enter a number 0-6:");
-		if(guess < 0 || guess > 6) {
+		if(guess < 0 || guess > 6) 
+		{
 			alert("Please enter a valid number!");
-		} else {
+		} 
+		else 
+		{
 			guesses = guesses + 1;
 		}
-		
+
+		if(guess == location1 || guess == location2 || guess == location3)
+		{
+			hits = hits + 1;
+
+			if (hits == 3)
+			{
+				isSunk = true;
+				alert("You sunk my Battleship!");
+			}
+		}
+
 }
