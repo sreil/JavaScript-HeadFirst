25d4fd782ed742287b1dcfe100cbd8e834b70406
sreil <seanpreilly@gmail.com>
2015-08-26 10:50:59 -0600

    update js file

--- Chapter 4/bubble.js
+++ Chapter 4/bubble.js
@@ -1,14 +1,20 @@
 var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
-34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61,
-46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
+		34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61,
+		46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
-var output;
+		var output;
+		var highScore = 0;
 
-var i = 0;
+		var i = 0;
 
-for (var i = 0; i < scores.length; i++)
-{
-	output = "Bubble solution #" + i + " score: " + scores[i];
-	console.log(output);
+		for (var i = 0; i < scores.length; i++)
+		{
+			output = "Bubble solution #" + i + " score: " + scores[i];
+			console.log(output);
 
-}
+			if (scores[i] > highScore) {
+				highScore = scores[i];
+			} 
+		}
+		console.log("Bubbles tests: " + i);
+		console.log("Highest bubble score: " + highScore);
