2bba9b2f4f67fad48c07c930ac8457ea7791c7ba
sreil <seanpreilly@gmail.com>
2015-08-27 21:57:48 -0600

    add fiat

---
+++ Chapter 5/fiat.js
@@ -0,0 +1,14 @@
+var fiat = {
+	make: "Fiat",
+	model: "500",
+	year: 1957,
+	color: "Medium Blue",
+	passengers: 2,
+	convertible: false,
+	mileage: 88000,
+	drive: function() {
+		alert.log("Zoom zoom!");
+	}
+};
+
+fiat.drive();
