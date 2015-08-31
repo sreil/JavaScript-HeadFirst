efc6f35fd6d8055066a407e5ce35f4c4c85b9fe9
sreil <seanpreilly@gmail.com>
2015-08-31 06:17:20 -0600

    update

--- Chapter 7/liedetector.js
+++ Chapter 7/liedetector.js
@@ -1,39 +1,39 @@
 function lieDetectorTest() {
 	var lies = 0;
-	var stolenDiamond = { };
+	var stolenDiamond = { }; //+1
 	if (stolenDiamond) {
 		console.log("You stole the diamond");
 		lies++;
 	}
 var car = {
-	keysInPocket: null
+	keysInPocket: null //+0
 };
 if (car.keysInPocket) {
 	console.log("Uh oh, guess you stole the car!");
 	lies++;
 }
-if (car.emptyGasTank) {
+if (car.emptyGasTank) { //+0
 	console.log("You drove the car after you stole it!");
 	lies++;
 }
-var foundYouAtTheCrimeScene = [ ];
+var foundYouAtTheCrimeScene = [ ]; //+1
 	if (foundYouAtTheCrimeScene) {
 		console.log("A sure sign of guilt");
 		lies++;
 }
-if (foundYouAtTheCrimeScene[0]) {
+if (foundYouAtTheCrimeScene[0]) { //+0
 	console.log("Caught with a stolen item!");
 	lies++;
 }
 var yourName = " ";
 	if (yourName) {
-		console.log("Guess you lied about your name");
+		console.log("Guess you lied about your name"); //+1
 		lies++;
 }
 return lies;
 }
-var numberOfLies = lieDetectorTest();
+var numberOfLies = lieDetectorTest(); //total is 3
 console.log("You told " + numberOfLies + " lies!");
 if (numberOfLies >= 3) {
-	console.log("Guilty as charged");
+	console.log("Guilty as charged"); //prints guilty
 }
