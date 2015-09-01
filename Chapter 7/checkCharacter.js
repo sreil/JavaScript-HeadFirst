149a16f6bb502c6ba0556105c0e200c6ca2236c8
sreil <seanpreilly@gmail.com>
2015-08-31 19:47:05 -0600

    update

--- Chapter 7/checkCharacter.js
+++ Chapter 7/checkCharacter.js
@@ -2,7 +2,7 @@ var input = "seanpreilly@gmail.com";
 
 for(var i = 0; i < input.length; i++) 
 {
-	if (input.charAt(i) = = = "@") 
+	if (input.charAt(i) === "@") 
 	{
 		console.log("There's an @ sign at index " + i);
 	}
