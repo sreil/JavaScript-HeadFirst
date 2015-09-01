7c5ed36f4f4f65c71c0a284ca9091bde6f861b8a
sreil <seanpreilly@gmail.com>
2015-08-31 19:41:12 -0600

    add string

---
+++ Chapter 7/string.js
@@ -0,0 +1,11 @@
+var name = "Jenny";
+var phone = "867-5309";
+var fact = "This is a prime number";
+
+var songName = phone + "/" + name;
+
+var index = phone.indexOf("-"); //convert to string object
+if(fact.substring(10,15) === "prime")
+{
+	alert(fact);
+}
