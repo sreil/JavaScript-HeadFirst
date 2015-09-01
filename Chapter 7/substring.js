65a3b23176bd1f4df92630924011cd565263f0d2
sreil <seanpreilly@gmail.com>
2015-08-31 20:40:24 -0600

    commented

--- Chapter 7/substring.js
+++ Chapter 7/substring.js
@@ -1,6 +1,6 @@
 var data = "name|phone|address";
 var val = data.substring(5, 10);
-console.log("Substring is " + val);
+console.log("Substring is " + val); //substring is phone
 
 val = data.substring(5);
-console.log("Substring is now " + val);
+console.log("Substring is now " + val); //substring is phone|address
