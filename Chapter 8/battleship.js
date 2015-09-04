96d0caedad927cb6c792b7ada0e1d2357db730c6
sreil <seanpreilly@gmail.com>
2015-09-03 19:42:15 -0600

    update

--- Chapter 8/battleship.js
+++ Chapter 8/battleship.js
@@ -6,10 +6,19 @@ var view {
 	},
 	displayHit: function(location)
 	{
-
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "hit");
 	},
 	displayMiss: function(location)
 	{
-
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
 	}
 };
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
