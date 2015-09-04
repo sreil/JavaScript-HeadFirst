d120f60e4219c8f6cfc24b30b95ef01625de9582
sreil <seanpreilly@gmail.com>
2015-09-04 08:22:43 -0600

    add ships

--- Chapter 8/battleship.js
+++ Chapter 8/battleship.js
@@ -16,6 +16,12 @@ var view {
 	}
 };
 
+var ships: [
+		{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+		{ locations: ["24", "34", "44"], hits: ["", "", ""] },
+		{ locations: ["10", "11", "12"], hits: ["", "", ""] }
+	];
+
 view.displayMiss("00");
 view.displayHit("34");
 view.displayMiss("55");
